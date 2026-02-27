f8f35af7f6538f8d897e518220388f56f9b657d3
Seferino Fernandez <seferinofz@gmail.com>
2026-02-26 19:27:17 -0700

    feat: Update to noaa-weather v1.1.0

--- Formula/noaa-weather.rb
+++ Formula/noaa-weather.rb
@@ -1,27 +1,27 @@
 class NoaaWeather < Formula
   desc "CLI for the NOAA Weather API"
   homepage "https://github.com/seferino-fernandez/noaa_weather"
-  version "0.1.8"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       # macOS Intel (x86_64) binary
-      url "https://github.com/seferino-fernandez/noaa_weather/releases/download/v0.1.8/noaa-weather-x86_64-apple-darwin.tar.gz"
-      sha256 "e17cb6c190fa5204f7c00fb787ae192c431f656449f09f42fc89df67755db3c3"
+      url "https://github.com/seferino-fernandez/noaa_weather/releases/download/v1.1.0/noaa-weather-x86_64-apple-darwin.tar.gz"
+      sha256 "3188fa5878fb7b82cfc18e6577223bd973521c179cd6d6504487f29e4f022ffe"
     end
     if Hardware::CPU.arm?
       # macOS Apple Silicon (aarch64) binary
-      url "https://github.com/seferino-fernandez/noaa_weather/releases/download/v0.1.8/noaa-weather-aarch64-apple-darwin.tar.gz"
-      sha256 "fb6f915e694e6ed5b97aeff7384c859b41553662610b3a8b0e7247909b5bae6a"
+      url "https://github.com/seferino-fernandez/noaa_weather/releases/download/v1.1.0/noaa-weather-aarch64-apple-darwin.tar.gz"
+      sha256 "e6f3355c33c38b83a68a822862dbdd131829627c31e861ab4f90fc49e0d29bd6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       # Linux x86_64 binary
-      url "https://github.com/seferino-fernandez/noaa_weather/releases/download/v0.1.8/noaa-weather-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4b64f411d4e1b1a3cfcf111394a57e4aee4f8e0fb52dc0573d8c5d8e6bc85a62"
+      url "https://github.com/seferino-fernandez/noaa_weather/releases/download/v1.1.0/noaa-weather-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "b56ff3b615e8cab7c9bf59c6a8e94272c794770607dfc9e30900108f47e45171"
     end
   end
 
